604726ca931dec4e3bffe2dbc61a60962b8c56d8
Anton Schuster <anton.k.schuster@gmail.com>
2021-03-17 18:23:41 -0500

    Initial Commit

---
+++ Climascale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climascale
+//
+//  Created by Anton Schuster on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
