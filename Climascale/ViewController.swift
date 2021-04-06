92ef519c7aca45dfb74064c165b619f4bda5ca17
Anton Schuster <anton.k.schuster@gmail.com>
2021-04-06 16:47:51 -0500

    test2 insta

--- Climascale/ViewController.swift
+++ Climascale/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("hello")
         print("Hi!")
         print("Test")
+        print("test2")
         // Do any additional setup after loading the view.
     }
 
