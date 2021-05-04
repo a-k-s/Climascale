65ad227fee6caa61562d73f144b555f82f491607
Adrian Reilly <adrianreilly582@gmail.com>
2021-05-04 12:40:29 -0500

    now can change baseline

--- Climascale/storyBoards/profileV2ViewController.swift
+++ Climascale/storyBoards/profileV2ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 import Firebase
 
 class profileV2ViewController: UIViewController {
@@ -25,6 +26,9 @@ class profileV2ViewController: UIViewController {
     
     @IBOutlet weak var surveyLabel: UILabel!
     @IBOutlet weak var surveyButton: UIButton!
+    
+    //new baseline goal
+    @IBOutlet var baselineTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,7 +67,13 @@ class profileV2ViewController: UIViewController {
             self.surveyButton.isHidden = true
             
         }
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
 
+    }
+    
+    @objc override func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -125,6 +135,78 @@ class profileV2ViewController: UIViewController {
             }
         
     }
+    
+    @IBAction func updateYourBaseLine(_ sender: Any) {
+        var newBaseline = Double(baselineTextField.text!) ?? 0
+        //round the number
+        let rounded = Double(round(10*newBaseline)/10)
+        //add commas
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let formattedNumber = numberFormatter.string(from: NSNumber(value:rounded))
+        
+        if Auth.auth().currentUser != nil {
+            let alert = UIAlertController(title: "Confirm new baseline", message: "Are you sure you want you want to set baseline to \(String(formattedNumber ?? "nil")) kg?", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { action in
+                switch action.style {
+                case .default:
+                    print("default")
+                case .cancel:
+                    print("cancel")
+                case .destructive:
+                    print("destructive")
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: { action in
+                switch action.style {
+                case .default:
+                    print("default")
+                    //sets new weekly goal and resets total
+                    if Auth.auth().currentUser != nil  {
+                        self.ref.child(self.myFunctions.getUidValue()).child("weeklygoal").getData { (error, snapshot) in
+                            if let error = error {
+                                print(error)
+                            }
+                            else if snapshot.exists() {
+                                self.ref.child(self.myFunctions.getUidValue()).child("weeklygoal").setValue(newBaseline)
+                                self.ref.child(self.myFunctions.getUidValue()).child("totalcarbonemissions").setValue(0)
+                                DispatchQueue.main.async {
+                                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainStart") as! UITabBarController
+                                    vc.modalPresentationStyle = .fullScreen
+                                    vc.modalTransitionStyle = .crossDissolve
+                                    self.present(vc, animated: true, completion: nil)
+                                }
+                                
+                            }
+                            else {
+                                print("no data")
+                            }
+                        }
+                    }
+                    else {
+                        
+                    }
+                    //if logged in
+                    //set weekly goal to minus 5 percent of baseline
+                    //set total back to 0
+                case .cancel:
+                    print("cancel")
+                case .destructive:
+                    print("destructive")
+                }
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }else {
+            
+        }
+    }
+    
+    
+    
+    
     
     
     
